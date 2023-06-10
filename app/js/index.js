78ce2e9a6c359907b4f3ecaff43e930b0b4eb188
max <89175221282@mail.ru>
2023-06-10 08:23:51 +0300

    создание карточек

---
+++ app/js/index.js
@@ -0,0 +1,50 @@
+/* import { menuClick } from "./module/menu.js";
+menuClick(); */
+
+const cardsPortfolio = [];
+function creatPortfolio() {
+    const cardImg=[];
+    const cardName = [`Landing vr.1`,`Landing vr.2`,`Landing vr.3`,`Portfolio 1`,`Portfolio 1`,`Portfolio 1`,
+    `Portfolio 1`,`Single Project`, `About Company`, `About TEam`, `Single Team Member`, `FAQ PAge`,
+    `Contact Us`, `Blog 1`,`Blog 2`,`Blog 3`,`Blog Categories`, `Single Blog`, `Pricing`, `Style guide`, `Licenses` ];
+    let weyCard = `./image/card/IMAGE (0).png`;
+    let IndexChar;
+    for (let index = 0; index < weyCard.length; index++) {
+        let char = weyCard[index]
+        if (char == `0`) {
+            IndexChar = index;
+        };
+    };
+
+    for (let index = 0; index < 21; index++) {
+        cardImg[index] = weyCard.slice(0,IndexChar)+index +weyCard.slice(IndexChar+1);
+    }
+    for (let index = 0; index < cardName.length; index++) {
+        cardsPortfolio.push({
+            img: cardImg[index],
+            name: cardName[index],
+        });
+        
+    };
+};
+creatPortfolio();
+console.log(cardsPortfolio);
+const parentDiv = document.querySelector(`.main__cards`);
+
+const writeCard = (cardsPortfolio , parentDiv) => {
+    parentDiv.innerHTML = cardsPortfolio.map ((card) =>
+        `
+        <div class="card">
+        <div class="card__img">
+            <img src="${card.img}" alt="">
+        </div>
+        <div class="card__item">
+            <p>${card.name}</p>
+        </div>
+    
+    </div>
+     `
+    ).join(``);
+
+};
+writeCard(cardsPortfolio, parentDiv);
---
+++ app/js/module/menu.js
@@ -0,0 +1,6 @@
+/* const menu = document.querySelector(".content_menu");
+const item = document.querySelector(".content_items");
+/* 
+ export minimenu= menu.addEventListener(`click` , => {
+        item
+        }); */ 
